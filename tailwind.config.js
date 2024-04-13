3cb60799cd1091f9c27948a736f23c7be5434a74
BeGesaMe <bekjonshavkatov0@gmail.com>
2024-04-13 22:02:27 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'black' : '#000000',
+        'card': '#F8F9FA'
+      }
+    },
+  },
+  plugins: [],
+}
